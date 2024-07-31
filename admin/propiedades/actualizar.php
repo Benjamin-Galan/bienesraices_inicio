f9e4a7e230ddc0785ac832f5e481e32e5da6fa93
Benjamin Galan <begalan@outlook.es>
2024-07-31 12:52:37 -0600

    Creando vistas actualizar y borrar

---
+++ admin/propiedades/actualizar.php
@@ -0,0 +1,13 @@
+<?php
+include '../../includes/funciones.php';
+
+incluirTemplate('header');
+?>
+
+<main class="contenedor seccion">
+    <h1>Actualizar</h1>
+</main>
+
+<?php
+incluirTemplate('footer');
+?>
