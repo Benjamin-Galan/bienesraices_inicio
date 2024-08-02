413e0d4ccf430df3cd25c6b9577b46023285043d
Benjamin Galan <begalan@outlook.es>
2024-08-02 01:25:26 -0600

    Obtener un query string y mostrando el resultado

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,17 @@
 <?php
-include '../includes/funciones.php';
 
+$resultado = $_GET['resultado'] ?? null;
+
+include '../includes/funciones.php';
 incluirTemplate('header');
 ?>
 
 <main class="contenedor seccion">
     <h1>Administrador de bienes raices</h1>
+
+    <?php if(intval($resultado) === 1): ?>
+        <p class="alerta exito">Anuncio creado correctamente</p>
+    <?php endif; ?>
 
     <a href="/bienesraices/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
 </main>
