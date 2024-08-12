3f6da55fb050c64795da9daa0220030647f3d69d
Benjamin Galan <begalan@outlook.es>
2024-08-12 13:04:58 -0600

    Añadiendo validacion de sesion de usuario

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,5 +1,12 @@
 <?php
 
+require '../../includes/funciones.php';
+$auth = estaAutenticado();
+
+if (!$auth) {
+    header('location: /bienesraices/index.php');
+}
+
 require '../../includes/config/database.php';
 $db = conectarDB();
 
@@ -104,7 +111,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-require '../../includes/funciones.php';
 incluirTemplate('header');
 ?>
 
